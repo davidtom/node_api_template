25bdd33f3b077157c263963d915e50ee784a8c23
davidtom <davidjtomczyk@gmail.com>
2017-11-19 20:16:47 -0500

    initial commit; express server is set up with basic middleware and skeleton code for a users route

---
+++ src/routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// handle all routes to /api/v1/users
+router.route('/')
+    .get(function(req, resp){
+        resp.json("hello!");
+    })
+    .post(function(req, resp){
+
+    });
+
+module.exports = router;
