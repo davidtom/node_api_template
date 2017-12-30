77da9d2a6a1097bd4612399065ba57cd5a2a5ba6
davidtom <davidjtomczyk@gmail.com>
2017-12-30 17:23:13 -0500

    create contacts middleware to check req.body for required data for a new contact and to add newContact property to req if so

---
+++ src/middlewares/contacts.js
@@ -0,0 +1,27 @@
+// Library Imports
+// Global Imports
+// Project Imports
+const AppError = require('../utils/appError');
+
+module.exports = {
+    contactDataPresent: function(req, res, next) {
+        // Pull data from request body
+        const { firstName, lastName, email, phone, source } = req.body;
+
+        // Check that all required data is present and set it to req.newContact if it is
+        if (email) {
+            const newContact = {
+                firstName,
+                lastName,
+                email,
+                phone,
+                source
+            };
+            req.newContact = newContact;
+            return next();
+        }
+        // Respond with an error if all required data is not present
+        const err = new AppError('Invalid body; new contact must have an email: ' + JSON.stringify(req.body), 400);
+        next(err);
+    }
+}
